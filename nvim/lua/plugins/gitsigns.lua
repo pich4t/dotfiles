e3e4a554cba05a86c1fb6179f556285a5e6d1c61
pich4t <picharatian@gmail.com>
2026-04-29 17:37:49 +0900

    gitsignsのnavigationを追加

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -8,6 +8,10 @@ return {
                 vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
             end
 
+            -- Navigation
+            map('n', ']c', function() gs.nav_hunk('next') end, 'Next hunk')
+            map('n', '[c', function() gs.nav_hunk('prev') end, 'Prev hunk')
+            -- Actuions
             map('n', '<Leader>gp', gs.preview_hunk_inline, 'Preview hunk')
             map('n', '<Leader>gr', gs.reset_hunk, 'Reset hunk')
             map('n', '<Leader>gs', gs.stage_hunk, 'Stage hunk')
