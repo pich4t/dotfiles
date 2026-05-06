fb28d191b9385be00f6d36f886437adebf9ecde8
pich4t <picharatian@gmail.com>
2026-05-07 06:37:12 +0900

    autopairs導入

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,2 +1,7 @@
 vim.keymap.set('n', '<Esc><Esc>', '<Cmd>noh<CR>', { desc = 'nohlsearch' })
 vim.keymap.set('n', '<Leader>l', '<Cmd>Lazy<CR>', { desc = 'Lazy' })
+-- vim.keymap.set({'n', 'v'}, 'j', 'h', { desc = 'Left' })
+-- vim.keymap.set({'n', 'v'}, 'k', 'j', { desc = 'Down' })
+-- vim.keymap.set({'n', 'v'}, 'l', 'k', { desc = 'Up' })
+-- vim.keymap.set({'n', 'v'}, ';', 'l', { desc = 'Right' })
+-- vim.opt.langmap = 'jh,kj,lk,\\;l'
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
         options = {
-            icons_enabled = false,
+            -- icons_enabled = false,
             theme = 'dracula'
         }
     }
