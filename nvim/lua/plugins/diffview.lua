ada3faa53a3e9eba812df1e2f3c08f94d859448a
pich4t <picharatian@gmail.com>
2026-08-09 19:24:58 +0900

    diffviewツリー上からCRでファイル内にカーソル移動できるよう変更

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,6 +1,15 @@
 return {
     'sindrets/diffview.nvim',
-    opts = {},
+    opts = function()
+        local actions = require('diffview.actions')
+        return {
+            keymaps = {
+                file_panel = {
+                    { 'n', '<CR>', actions.focus_entry, { desc = 'Open the diff and focus it' } },
+                },
+            },
+        }
+    end,
     keys = {
         { '<Leader>gd', '<Cmd>DiffviewOpen<CR>', desc = 'Open diff view' },
         { '<Leader>gh', '<Cmd>DiffviewFileHistory %<CR>', desc = 'File history' },
